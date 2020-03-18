30c079852567aa97e71c8b73ef532c5f77a7926a
Ng Weng Hoe <wenghoeng@gmail.com>
2020-03-18 09:06:55 +0800

    Add more model tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,6 +4,6 @@ RSpec.describe User, type: :model do
   it { is_expected.to validate_presence_of(:first_name) }
   it { is_expected.to validate_presence_of(:last_name) }
   it { is_expected.to validate_presence_of(:email) }
-  #it { is_expected.to have_many(:thoughts) }
+  it { is_expected.to have_many(:thoughts) }
 
 end
