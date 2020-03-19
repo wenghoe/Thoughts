03eecdcaeb30fa75203b8a4be18be55adbd585ba
Ng Weng Hoe <wenghoeng@gmail.com>
2020-03-19 10:03:16 +0800

    Fix model specs

--- spec/models/hashtag_spec.rb
+++ spec/models/hashtag_spec.rb
@@ -4,5 +4,5 @@ require 'rails_helper'
 
 RSpec.describe Hashtag, type: :model do
   it { is_expected.to have_many(:thought_hashtags) }
-  it { is_expected.to have_many(:thoughts).through(thought_hashtags) }
+  it { is_expected.to have_many(:thoughts).through(:thought_hashtags) }
 end
--- spec/models/thought_spec.rb
+++ spec/models/thought_spec.rb
@@ -6,5 +6,5 @@ RSpec.describe Thought, type: :model do
   it { is_expected.to validate_presence_of(:content) }
   it { is_expected.to belong_to(:user) }
   it { is_expected.to have_many(:thought_hashtags) }
-  it { is_expected.to have_many(:hashtags).through(thought_hashtags) }
+  it { is_expected.to have_many(:hashtags).through(:thought_hashtags) }
 end
